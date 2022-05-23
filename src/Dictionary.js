6f9289d31a6ce9d8275c09f71c236c427f5d7d55
Victoria123654 <krasikova.vg@gmail.com>
2022-05-22 21:18:51 -0700

    added pics

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import "./Dictionary.css"
 import axios from "axios";
 import Results from "./Results";
-import "./Results.css"
+import Photos from "./Photos"
 
 
 export default function Dictionary(props) {
     let [keyword, setKeyword] = useState(props.defaultKeyword)
     let [results, setResults] = useState(null)
     let [loaded, setLoaded] = useState(false)
+    let [photos, setPhotos] = useState(null)
     function handleResponse(response) {
 
         setResults(response.data[0])
@@ -18,10 +19,17 @@ export default function Dictionary(props) {
         event.preventDefault()
         search()
     }
+    function handlePexelResponse(response) {
+        setPhotos(response.data.photos)
+
+    }
 
     function search() {
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`
         axios.get(apiUrl).then(handleResponse);
+        let apiKey = "563492ad6f9170000100000114a0522419624fb694d5e583a3a37df6"
+        let pexelsApi = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`
+        axios.get(pexelsApi, { headers: { "Authorization": `Bearer ${apiKey}` } }).then(handlePexelResponse)
     }
     function updateKeyword(event) {
         setKeyword(event.target.value)
@@ -41,6 +49,7 @@ export default function Dictionary(props) {
                     </form>
                 </section>
                 <Results info={results} />
+                <Photos photos={photos} />
             </div>)
     }
     else {
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./Phonetics.css"
 
 export default function Phonetic(props) {
-    console.log(props.phonetic.audio)
     return (
         <div className="Phonetic">
             <audio controls preload="auto">
